d9606322ad06de5071763b673dede49da8038b9f
Manjit Kumbhakar <manjitkumbhakar@gmail.com>
2022-06-28 22:24:28 +0530

    added some important text for final.cpp

--- final.cpp
+++ final.cpp
@@ -17,7 +17,7 @@ int findUnique(int array[],int size){
     cin>>size;
     
     int arr[size];
-    cout<<"Enter the element of the array "<<endl;
+    cout<<"Enter the elements of the array "<<endl;
     for(int i=0; i<size; i++){
         cin>>arr[i];
     }
